b378edb3a87141b83867b4f833a55da8bcb3dc9a
KunwarVSuryavanshi <virajkunwar98@gmail.com>
2022-12-08 02:50:58 +0530

    :sparkle: banner animation

--- src/Components/BannerCarousel/BannerCarousel.jsx
+++ src/Components/BannerCarousel/BannerCarousel.jsx
@@ -1,21 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./BannerCarousel.scss";
 
 function Carousel(props) {
-  useEffect(() => {});
+  const [index, setIndex] = useState(0);
+  let interval = useRef();
+
+  useEffect(() => {
+    let temp = 0; // Closure beesh :P
+    interval.current = setInterval(() => {
+      if (temp < props.data.length-1) {
+        temp++;
+        setIndex((prev) => prev + 1);
+      }
+      else {
+        temp = 0;
+        setIndex(0);
+      }
+    }, 5000);
+    return () => {
+      clearInterval(interval.current);
+    };
+  }, [props?.data?.length]);
 
   return (
     <>
+      {console.log("Interval--->", interval, index)}
       {props?.data?.map((item, key) => {
         return (
-          <div className={`container`}>
-            <div className="container_gradient"
+          <div
+            className={`container ${key !== index ? "hide_img" : "show_img"}`}
+          >
+            <div
+              className="container_gradient"
               style={{
                 background: "linear-gradient(90deg,rgba(0,0,0,.8),transparent)",
               }}
             >
               <div
-                className="container_img"
+                className={`container_img`}
                 style={{ backgroundImage: `url(${item?.cover})` }}
               ></div>
             </div>
