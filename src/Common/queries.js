119a682b818d26b8d9afb766a65028537ac072df
KunwarVSuryavanshi <virajkunwar98@gmail.com>
2022-12-14 01:36:31 +0530

    :zap: airingQuery

--- src/Common/queries.js
+++ src/Common/queries.js
@@ -1,41 +1,92 @@
 // Anilist
 
-export const anilistNotAiringScheduleQuery = (date, page = 1, perPage = 50) =>
-  `query { 
-  Page(page: ${page}, perPage: ${perPage}) { 
-    pageInfo { 
-      total 
-      perPage 
-      currentPage 
-      lastPage 
-      hasNextPage
-    } 
-    media(countryOfOrigin: JP, status: NOT_YET_RELEASED, startDate_greater: ${
-      date.getFullYear() + "" + date.getMonth() + "" + date.getDate()
-    }){
-      title {
-        romaji
-        english
-        native
-        userPreferred
-      },
-      startDate {
-        year
-        month
-        day
-      },
-      trailer {
-        id
-      },
-      coverImage {
-        extraLarge
-        large
-        medium
-        color
+export const anilistNotAiringScheduleQuery = (date, page = 1, perPage = 50) => {
+  return (
+    `query { 
+      Page(page: ${page}, perPage: ${perPage}) { 
+        pageInfo { 
+          total 
+          perPage 
+          currentPage 
+          lastPage 
+          hasNextPage
+        } 
+        media(countryOfOrigin: JP, status: NOT_YET_RELEASED, startDate_greater: ${date.getFullYear() + "" + date.getMonth() + "" + date.getDate()}){
+          title {
+            romaji
+            english
+            native
+            userPreferred
+          },
+          startDate {
+            year
+            month
+            day
+          },
+          trailer {
+            id
+          },
+          coverImage {
+            extraLarge
+            large
+            medium
+            color
+          }
+        }
       }
-    }
-  }
-}`;
+    }`
+  )
+};
+export const airingToday = (date, page = 1, perPage = 50) => {
+  return `query { 
+      Page(page: ${page}, perPage: ${perPage}) { 
+        pageInfo { 
+          total 
+          perPage 
+          currentPage 
+          lastPage 
+          hasNextPage
+        } 
+        media(countryOfOrigin: JP, status: RELEASING, sort: TRENDING_DESC){
+          id,
+          idMal,
+          type,
+          format,
+          description,
+          startDate {
+            year
+            month
+            day
+          },
+          endDate {
+            year
+            month
+            day
+          },
+          episodes,
+          title {
+            romaji
+            english
+            native
+            userPreferred
+          },
+          coverImage {
+            extraLarge
+            large
+            medium
+            color
+          },
+          bannerImage,
+          genres,
+          averageScore,
+          meanScore,
+          nextAiringEpisode {
+            id
+          }
+        }
+      }
+    }`;
+};
 
 //   `query {
 //   Page(page: ${page}, perPage: ${perPage}) {
--- src/Common/utils.js
+++ src/Common/utils.js
@@ -1,3 +1,17 @@
 export const cleanHTML = (html) => {
   return html.replace(/<[^>]*>?/gm, "");
 };
+
+export const getSeason = () => {
+  const date = new Date().getMonth();
+  if (date >= 2 && date <= 4) {
+    return 'SPRING'
+  }
+  if (date >= 5 && date <= 7) {
+    return 'SUMMER'
+  }
+  if (date >= 8 && date <= 10) {
+    return 'FALL'
+  }
+  return 'WINTER'
+}
