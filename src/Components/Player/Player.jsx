d5397b3301fdd830a39ab8b950c5126003669790
KunwarVSuryavanshi <virajkunwar98@gmail.com>
2022-12-21 05:06:58 +0530

    feat: :zap: about page for anime :zap:

--- src/Components/Player/Player.jsx
+++ src/Components/Player/Player.jsx
@@ -1,22 +1,29 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import ReactPlayer from 'react-player';
-import { useLocation, useParams } from 'react-router-dom'
-import './Player.scss';
+import { Chip } from "@mui/material";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import ReactPlayer from "react-player";
+import { useLocation, useParams } from "react-router-dom";
+import "./Player.scss";
+import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import PlaylistPlayIcon from "@mui/icons-material/PlaylistPlay";
+import CellTowerIcon from "@mui/icons-material/CellTower";
+import { cleanHTML } from "../../Common/utils";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
 
 function Player() {
   const { epInfo } = useParams();
   const state = useLocation();
   const [animeInfo, setAnimeInfo] = useState(null);
-
+  const formatter = Intl.NumberFormat("en", { notation: "compact" });
   useEffect(() => {
     if (epInfo) {
       axios
         .get(`https://api.consumet.org/meta/anilist/info/${epInfo}`)
-        .then(res => setAnimeInfo(res.data))
+        .then((res) => setAnimeInfo(res.data));
     }
-  }, [epInfo])
-  
+  }, [epInfo]);
+
   return (
     <div className="player_root">
       {console.log(animeInfo)}
@@ -32,23 +39,54 @@ function Player() {
                 style={{ backgroundImage: `url(${animeInfo?.image})` }}
               />
             </div>
-            <div className="about">
-              <div className="title">
-                {animeInfo?.title?.romaji ?? animeInfo?.title?.english}
+            {animeInfo && (
+              <div className="about">
+                <div className="title">
+                  {animeInfo?.title?.native && (
+                    <div className="title_jp">
+                      {animeInfo?.title?.native} || &nbsp;
+                    </div>
+                  )}
+                  {animeInfo?.title?.romaji ?? animeInfo?.title?.english}
+                </div>
+                <div className="anime_info">
+                  <div className="type">{animeInfo?.type}</div>
+                  <div className="score">
+                    <ThumbUpOffAltIcon /> &nbsp;
+                    {animeInfo?.rating / 10 ?? "NA"}
+                  </div>
+                  <div className="status">
+                    <CellTowerIcon /> &nbsp;{animeInfo?.status}
+                  </div>
+                  <div className="ep_count">
+                    <PlaylistPlayIcon /> &nbsp;
+                    {animeInfo?.totalEpisodes}
+                  </div>
+                  <div className="duration">
+                    <AccessTimeIcon /> &nbsp;
+                    {animeInfo?.duration}
+                  </div>
+                  <div className="popularity">
+                    <TrendingUpIcon /> &nbsp;
+                    {formatter.format(animeInfo?.popularity)}
+                  </div>
+                </div>
+                <div className="description">
+                  {cleanHTML(animeInfo?.description) ?? "NA"}
+                </div>
+                {/* <div className="popularity">{animeInfo?.poularity}</div>*/}
+                <div className="genres">
+                  {animeInfo?.genres
+                    ?.slice(0, 6)
+                    ?.map(
+                      (item) => item && <Chip label={item} color="secondary" />
+                    )}
+                </div>
+                {/* <div className="studio" title='Studio'>
+                  {animeInfo?.studios?.[0]}
+                </div> */}
               </div>
-              <div className="score">{animeInfo?.rating / 10 ?? "NA"}</div>
-              <div className="description">
-                {animeInfo?.description ?? "NA"}
-              </div>
-              <div className="status">{animeInfo?.status}</div>
-              <div className="format">{animeInfo?.format}</div>
-              {/* <div className="popularity">{animeInfo?.poularity}</div>*/}
-              <div className="genres">
-                {animeInfo?.genres?.map((item) => (
-                  <span>{item}</span>
-                ))}
-              </div>
-            </div>
+            )}
           </div>
         </div>
         <div className="genres"></div>
@@ -70,4 +108,4 @@ function Player() {
   );
 }
 
-export default Player
+export default Player;
