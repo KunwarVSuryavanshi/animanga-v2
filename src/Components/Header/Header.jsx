3414cb4660f887bb621b7ba4477261741206a5a4
KunwarVSuryavanshi <virajkunwar98@gmail.com>
2022-12-27 02:45:08 +0530

    :zap: responsive for mobile screen :zap:

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.scss';
+import SearchIcon from "@mui/icons-material/Search";
 
 function Header() {
   return (
     <div className="header">
       <div className="header_logo">
-        <Link to={'/'} className="link">Animanga</Link>
+        <Link to={"/"} className="link">
+          Animanga
+        </Link>
       </div>
       <div className="header_nav">
         <div className="header_nav-links">
@@ -20,6 +23,7 @@ function Header() {
           </Link>
         </div>
       </div>
+      <div className="searchbar"><SearchIcon/></div>
     </div>
   );
 }
